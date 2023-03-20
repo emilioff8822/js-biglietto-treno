b5f67d45fdc5cf7fb73be880e502556001373241
emilioff8822 <emilioff@hotmail.it>
2023-03-20 15:39:23 +0100

    comm-pt1

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+Passaggi
+
+1 chiedi e salva in una variabile (numKm) il numero di chilimetri che si vuole percorrere
+
+2 chiedi e salva in una variabile l'eta (age) del passeggero
+
+3 salva il prezzo di ogni kilometro di viaggio (0.21 eur ) in una variabile chiamata prezzoKm
+
+4 moltiplica la variabile prezzokm  per il numero totale di km da percorrere (numKm)  ed ottieni un prezzo finale.
+Salva questo risultato in una variabile chiamata prezzoFin1
+
+5 se age del passeggero e' minore di 18 anni
+prezzoFin1 viene scontato del 20%
+
+6 se age del passeggero e' maggiore di 65 anni
+prezzoFin1 viene scontato del 20%
---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+1 chiedi e salva in una variabile (numKm) il numero di chilimetri che si vuole percorrere
+
+2 chiedi e salva in una variabile l'eta (age) del passeggero
+
+3 salva il prezzo di ogni kilometro di viaggio (0.21 eur ) in una variabile chiamata prezzoKm
+
+4 moltiplica la variabile prezzokm  per il numero totale di km da percorrere (numKm)  ed ottieni un prezzo finale.
+Salva questo risultato in una variabile chiamata prezzoFin1
+
+5 se age del passeggero e' minore di 18 anni
+prezzoFin1 viene scontato del 20% , inserito e salvato in una  variabile chiamata prezzonFin2
+
+6 se age del passeggero e' maggiore di 65 anni
+prezzoFin1 viene scontato del 20% inserito e salvato in una  variabile chiamata prezzonFin3
+
+
+*/
+
+const numKm = prompt('Inserisci il numero di Km da percorrere');
+
+const age = prompt('Inserisci la tua eta');
+
+console.log(num);
+
+const prezzoKm = 0.21;
+
+let prezzoFin1 = numKm * prezzoKm;
+
+console.log(prezzoFin1);
